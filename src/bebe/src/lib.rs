a3b79f36563300ae975c81b1100503bb4ade2f6b
voodookiidoo <voodookiidoo@yandex.ru>
2025-11-02 03:30:57 +0500

    this shit actually works now!

--- src/bebe/src/lib.rs
+++ src/bebe/src/lib.rs
@@ -3,60 +3,22 @@ mod util;
 
 use proc_macro::TokenStream;
 use quote::quote;
-use std::any::TypeId;
-use std::arch::naked_asm;
-use std::collections::{HashMap, HashSet};
-use syn::{FnArg, ImplItem, ItemImpl, ItemStruct, ReturnType, Type, parse_macro_input};
-
-#[proc_macro_attribute]
-pub fn component(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(item as ItemStruct);
-    let name = &input.ident;
-
-    let g = quote! {
-        #input
-        impl Default for #name {
-            fn default() -> Self {
-                Self {}
-            }
-        }
-
-        inventory::submit! {
-            #![crate = bebe]
-            bebe::di::ComponentRegistration::new::<#name>()
-        }
-        inventory::collect!{
-
-        }
-    };
-
-    g.into()
-}
+use syn::{FnArg, ItemImpl, ReturnType, Type, parse_macro_input};
 
 const BEAN_IDENTIFIER: &'static str = "bean";
 
-#[proc_macro_attribute]
-pub fn config(_attr: TokenStream, item: TokenStream) -> TokenStream {
+// #[proc_macro_attribute]
+fn bean(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    item.into()
+}
+// #[proc_macro_attribute]
+fn config(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let input = parse_macro_input!(item as ItemImpl);
-    let mut bean_graph: HashMap<_, Vec<_>> = Default::default();
-    for item in input.items {
-        // если это функция
-        if let ImplItem::Fn(f) = item {
-            // и функция помечена как bean
-            let output_type = if let Some(t) = get_output_type_of_sig(&f.sig) {
-                t
-            } else {
-                continue;
-            };
-            let input_types = get_input_types_of_sig(&f.sig);
-            bean_graph.insert(output_type, input_types);
-        }
-    }
     todo!()
 }
 
 fn get_output_type_of_sig(sig: &syn::Signature) -> Option<Type> {
-    if let ReturnType::Type(a, b) = &sig.output {
+    if let ReturnType::Type(_, b) = &sig.output {
         let t = *(b.clone());
         Some(t)
     } else {
@@ -78,3 +40,56 @@ fn get_input_types_of_sig(sig: &syn::Signature) -> Vec<Type> {
         .collect::<Vec<_>>();
     input_types
 }
+
+#[proc_macro_derive(Component, attributes(wired))]
+pub fn component(item: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(item as syn::DeriveInput);
+    let name = &input.ident;
+
+    // список полей с #[wired]
+    let mut wired_fields = Vec::new();
+    let mut dep_types = Vec::new();
+
+    if let syn::Data::Struct(ds) = &input.data {
+        if let syn::Fields::Named(fields) = &ds.fields {
+            for field in &fields.named {
+                if field.attrs.iter().any(|a| a.path().is_ident("wired")) {
+                    let ident = field.ident.as_ref().unwrap();
+                    let ty = &field.ty;
+                    wired_fields.push((ident.clone(), ty.clone()));
+                    dep_types.push(quote! { || std::any::TypeId::of::<#ty>() });
+                }
+            }
+        }
+    }
+
+    // имя фабрики
+    let ctor_fn = syn::Ident::new(&format!("__create_{}", name), name.span());
+    let def_name = syn::Ident::new(&format!("__DEF_{}", name), name.span());
+    let field_idents: Vec<_> = wired_fields.iter().map(|(i, _)| i).collect();
+    let field_types: Vec<_> = wired_fields.iter().map(|(_, t)| t).collect();
+
+    let g = quote! {
+        // #input
+
+        fn #ctor_fn(ctx: &Context) -> Box<dyn std::any::Any> {
+            let instance = #name {
+                #(
+                    #field_idents: ctx.get::<#field_types>().unwrap().clone(),
+                )*
+            };
+            Box::new(instance)
+        }
+
+        #[linkme::distributed_slice(ALL_BEANS)]
+        static #def_name: ComponentDef = ComponentDef {
+            bean_type: || std::any::TypeId::of::<#name>(),
+            deps: &[ #( #dep_types ),* ],
+            name: stringify!(#name),
+            ctor: #ctor_fn,
+        };
+    };
+    g.into()
+}
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,132 @@
-use std::task::Context;
-use std::{
-    any::{Any, TypeId},
-    collections::HashMap,
-    sync::OnceLock,
-};
+use bebe::Component;
+use linkme::distributed_slice;
+use std::any::{Any, TypeId};
+use std::collections::{HashMap, HashSet, VecDeque};
+use std::hash::Hash;
 
-pub struct ApplicationContext {
-    beans: HashMap<TypeId, BeanDefinition>,
+#[distributed_slice]
+pub static ALL_BEANS: [ComponentDef] = [..];
+
+#[derive(Debug)]
+pub struct ComponentDef {
+    pub bean_type: fn() -> TypeId,
+    pub name: &'static str,
+    pub deps: &'static [fn() -> TypeId], // зависимости
+    pub ctor: fn(&Context) -> Box<dyn Any>,
 }
 
-type FactoryFn = fn(&ApplicationContext) -> Box<dyn Any>;
-
-struct BeanDefinition {
-    factory: FactoryFn,
-    instance: OnceLock<&'static dyn Any>,
+#[derive(Debug)]
+pub struct Context {
+    beans: HashMap<TypeId, Box<dyn Any>>,
 }
 
-impl ApplicationContext {
+impl Context {
     pub fn new() -> Self {
         Self {
             beans: HashMap::new(),
         }
     }
 
-    pub fn register<T: 'static>(&mut self, factory: FactoryFn) {
-        self.beans.insert(
-            TypeId::of::<T>(),
-            BeanDefinition {
-                factory,
-                instance: OnceLock::new(),
-            },
-        );
+    fn build_dependency_graph(&self) -> HashMap<TypeId, HashSet<TypeId>> {
+        let mut graph = HashMap::new();
+        for def in ALL_BEANS {
+            let key = (def.bean_type)();
+            let deps = def.deps.iter().map(|f| f()).collect::<HashSet<_>>();
+            graph.insert(key, deps);
+        }
+        graph
     }
 
-    pub fn get<T: 'static>(&self) -> &'static T {
-        let def = self
-            .beans
+    pub fn init(&mut self) {
+        // создаём все компоненты в порядке регистрации
+        let graph = self.build_dependency_graph();
+        let result = topo_sort(graph).expect("Cycle detected while building dependency graph!");
+        for def in result.into_iter().rev() {
+            if let Some(def) = ALL_BEANS.iter().find(|d| (d.bean_type)() == def) {
+                let instance = (def.ctor)(self);
+                self.beans.insert((def.bean_type)(), instance);
+            }
+        }
+    }
+
+    pub fn get<T: 'static>(&self) -> Option<&T> {
+        self.beans
             .get(&TypeId::of::<T>())
-            .expect(&format!("Bean {} not found", std::any::type_name::<T>()));
+            .and_then(|b| b.downcast_ref())
+    }
 
-        def.instance
-            .get_or_init(|| {
-                // 1. создаём Box<dyn Any>
-                let boxed_any = (def.factory)(self);
-                // 2. превращаем в &'static dyn Any
-                Box::leak(boxed_any)
-            })
-            .downcast_ref::<T>()
-            .expect(&format!(
-                "Bean registered under type {}, but type mismatch occurred",
-                std::any::type_name::<T>()
-            ))
+    pub fn get_mut<T: 'static>(&mut self) -> Option<&mut T> {
+        self.beans
+            .get_mut(&TypeId::of::<T>())
+            .and_then(|b| b.downcast_mut())
     }
 }
 
-struct SomeConfig;
+#[derive(Component, Clone)]
+struct SmallService;
 
-#[bebe::config]
-impl SomeConfig{
-    
+impl SmallService {
+    fn call1(&self) {
+        println!("small service executed");
+    }
 }
-fn main() {
+#[derive(Component)]
+struct BigService {
+    #[wired]
+    small_service: SmallService,
+}
 
+impl BigService {
+    fn call2(&self) {
+        println!("big service started");
+        self.small_service.call1();
+        println!("big service finished");
+    }
+}
+
+
+fn main() {
+    let mut ctx = Context::new();
+    ctx.init();
+    ctx.get::<BigService>().unwrap().call2();
+}
+
+pub fn topo_sort<T: Hash + Eq + Clone + PartialEq>(
+    map: HashMap<T, HashSet<T>>,
+) -> Option<VecDeque<T>> {
+    let mut indegrees = map
+        .keys()
+        .map(|it| (it.to_owned(), 0))
+        .collect::<HashMap<_, _>>();
+    for dep in map.values() {
+        for d in dep {
+            if let Some(v) = indegrees.get_mut(d) {
+                *v += 1;
+            };
+        }
+    }
+    let mut q = map
+        .keys()
+        .filter(|it| indegrees[*it] == 0)
+        .cloned()
+        .collect::<VecDeque<_>>();
+    let mut final_order: VecDeque<T> = Default::default();
+
+    while let Some(curr) = q.pop_front() {
+        final_order.push_back(curr.clone());
+        for dep in &map[&curr] {
+            if let Some(v) = indegrees.get_mut(&dep.clone()) {
+                *v -= 1;
+                if *v == 0 {
+                    q.push_back(dep.clone());
+                }
+            }
+        }
+    }
+
+    if final_order.len() == map.len() {
+        Some(final_order)
+    } else {
+        None
+    }
 }
